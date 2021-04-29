ac8cd0cf820c544b67647af8cb1115b4f39e1f36
agrinmanriv0537 <75352922+agrinmanriv0537@users.noreply.github.com>
2021-04-29 16:41:40 -0500

    first commit

---
+++ src/main/include/Vision/VisionManager.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Networktables/NetworkTable.h"
+#include "Networktables/NetworkTableInstance.h"
+
+class VisionManager
+{
+    public:
+    double 
+      disX = 0
+    , disY = 0
+    , tx = 0
+    , ty = 0
+    , ta = 0
+    , tv = 0;
+
+    bool targetFound = false;
+    void Update();
+    VisionManager();
+    void TurnOffLights();
+    std::shared_ptr<NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
+};
